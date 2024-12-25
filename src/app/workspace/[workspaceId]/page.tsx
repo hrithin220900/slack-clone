6a031738114c2ddf97cc75a18e7a9b007394eb75
hrithin220900 <65213514+hrithin220900@users.noreply.github.com>
2024-12-25 10:12:12 +0530

    feat(workspace/[workspaceId]): setup toolbar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,10 +1,14 @@
-interface WorkspaceIdPageProps {
-  params: {
-    workspaceId: string;
-  };
-}
-const WorkspaceIdPage = ({ params }: WorkspaceIdPageProps) => {
-  return <div>ID: {params.workspaceId}</div>;
+"use client";
+
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+const WorkspaceIdPage = () => {
+  const workspaceId = useWorkspaceId();
+
+  const { data } = useGetWorkspace({ id: workspaceId });
+
+  return <div>ID: {JSON.stringify(data)}</div>;
 };
 
 export default WorkspaceIdPage;
