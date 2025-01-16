65ca7a31b02a61df9883682b2e0867dafa4ff47a
hrithin220900 <65213514+hrithin220900@users.noreply.github.com>
2025-01-16 09:57:42 +0530

    feat(workspaces/[workspaceId]/channel/[channelId]): setup channel header

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -56,18 +56,18 @@ const WorkspaceIdPage = () => {
     workspaceId,
   ]);
 
-  if (workspaceLoading || channelsLoading) {
+  if (workspaceLoading || channelsLoading || memberLoading) {
     return (
       <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
-        <Loader className="dize-6 animate-spin text-muted-foreground" />
+        <Loader className="size-5 animate-spin text-muted-foreground" />
       </div>
     );
   }
 
-  if (!workspace) {
+  if (!workspace || !member) {
     return (
       <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
-        <TriangleAlertIcon className="size-6 text-muted-foreground" />
+        <TriangleAlertIcon className="size-5 text-muted-foreground" />
         <span className="text-sm text-muted-foreground">
           Workspace not found
         </span>
